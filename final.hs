b8ad61df374bf048fc174d1665b5348c591b50a2
masonxotto <masonxotto@gmail.com>
2023-05-02 13:56:51 -0500

    minor changes to typeof, also added typeof for Fix had to change Lambda declaration from (Lambad i b) to (Lambad i d b) for eval. Trying to figure out issue with typeof for Lambda

--- final.hs
+++ final.hs
@@ -11,7 +11,6 @@ data KUTypeLang where
 data KULang where
     Num :: Int -> KULang  
     Boolean :: Bool -> KULang
-    -- (:->:) :: KULangVal -> KULangVal -> KULang
     Plus :: KULang -> KULang -> KULang 
     Minus :: KULang -> KULang -> KULang
     Mult :: KULang -> KULang -> KULang 
@@ -19,7 +18,7 @@ data KULang where
     Exp :: KULang -> KULang -> KULang
     If0 :: KULang -> KULang -> KULang -> KULang
     Id :: String -> KULang
-    Lambda :: String -> KULang -> KULang 
+    Lambda :: String -> KUTypeLang -> KULang -> KULang 
     App :: KULang -> KULang -> KULang 
     And :: KULang -> KULang -> KULang   
     Or :: KULang -> KULang -> KULang  
@@ -35,14 +34,13 @@ data KULangVal where
     NumV :: Int -> KULangVal
     BooleanV :: Bool -> KULangVal
     ClosureV :: String -> KULang -> EnvVal -> KULangVal
-    (:->:) :: KULangVal -> KULangVal -> KULangVal
     UnitV :: KULangVal -- New Feature
     deriving (Show,Eq)
 
 -- Environment & Gamma Definitions
 type Env = [(String,KULang)]
 type EnvVal = [(String,KULangVal)]
-type Gamma = [(string, KUTypeLang)]
+type Gamma = [(String, KUTypeLang)]
 
 -- Reader Definition
 data Reader e a = Reader (e -> a)
@@ -97,76 +95,77 @@ subst i v (Bind i' v' b') = if i == i'
     then Bind i' (subst i v v') b'
     else Bind i' (subst i v v') (subst i v b')
 subst i v (Fix f) = Fix (subst i v f)
-subst x t (Lambda i b) = if x==i 
-  then Lambda i b
-  else Lambda i (subst x t b)
+subst x t (Lambda i d b) = if x==i 
+  then Lambda i d b
+  else Lambda i d (subst x t b)
 subst x t (App f a) = App (subst x t f) (subst x t a)
 
 
 -- Implementing Types
-typeofMonad :: Gamma -> KULang -> Maybe KUTypeLang
-typeofMonad _ (Num n) = if n>=0 then return TNum else Nothing
-typeofMonad _ (Boolean b) = return TBool
-typeofMonad g (Plus l r) = do {TNum <- typeofMonad g l;
-                             TNum <- typeofMonad g r;
+typeof :: Gamma -> KULang -> Maybe KUTypeLang
+typeof _ (Num n) = if n>=0 then return TNum else Nothing
+typeof _ (Boolean b) = return TBool
+typeof g (Plus l r) = do {TNum <- typeof g l;
+                             TNum <- typeof g r;
                              return TNum}
                              
-typeofMonad g (Minus l r) = do {TNum <- typeofMonad g l;
-                              TNum <- typeofMonad g r;
+typeof g (Minus l r) = do {TNum <- typeof g l;
+                              TNum <- typeof g r;
                               return TNum}
 
-typeofMonad g (Mult l r) = do {TNum <- typeofMonad g l;
-                             TNum <- typeofMonad g r;
+typeof g (Mult l r) = do {TNum <- typeof g l;
+                             TNum <- typeof g r;
                              return TNum}
 
-typeofMonad g (Div n d) = do {TNum <- typeofMonad g n;
-                            TNum <- typeofMonad g d;
+typeof g (Div n d) = do {TNum <- typeof g n;
+                            TNum <- typeof g d;
                             return TNum}
 
-typeofMonad g (Exp x n) = do {TNum <- typeofMonad g x;
-                            TNum <- typeofMonad g n;
+typeof g (Exp x n) = do {TNum <- typeof g x;
+                            TNum <- typeof g n;
                             return TNum}
 
-typeofMonad g (Id x) = lookup x g 
+typeof g (Id x) = lookup x g 
 
-typeofMonad g (Lambda i d b) = do {r <- typeofMonad (i,d):g b;
-                                    return d :->: r}
+typeof g (Lambda i d b) = do {r <- typeof (i,d):g b;
+                              return d (:->:) r}
 
-typeofMonad g (App f a) = do {a' <- typeofMonad g a;
-                              d :->: r <- typeofMonad g f;
+typeof g (App f a) = do {a' <- typeof g a;
+                              d :->: r <- typeof g f;
                               if a'==d then return r else Nothing}
 
-typeofMonad g (And l r) = do {TBool<-typeofMonad g l;
-                            TBool<-typeofMonad g r;
+typeof g (And l r) = do {TBool<-typeof g l;
+                            TBool<-typeof g r;
                             return TBool}
 
-typeofMonad g (Or l r) = do {TBool<-typeofMonad g l;
-                            TBool<-typeofMonad g r;
+typeof g (Or l r) = do {TBool<-typeof g l;
+                            TBool<-typeof g r;
                             return TBool}
 
-typeofMonad g (Leq l r) = do {TNum<-typeofMonad g l;
-                            TNum<-typeofMonad g r;
+typeof g (Leq l r) = do {TNum<-typeof g l;
+                            TNum<-typeof g r;
                             return TBool}
 
-typeofMonad g (IsZero x) = do{TNum<-typeofMonad g x;
+typeof g (IsZero x) = do{TNum<-typeof g x;
                             return TBool}
 
-typeofMonad g (If c t e) = do{TBool<-typeofMonad g c;
-                            t'<- typeofMonad g t;
-                            e'<- typeofMonad g e;
+typeof g (If c t e) = do{TBool<-typeof g c;
+                            t'<- typeof g t;
+                            e'<- typeof g e;
                             if t' == e' then return t' else Nothing}
 
-typeofMonad g (Between s m e) = do{TNum<- typeofMonad g s;
-                                 TNum<- typeofMonad g m;
-                                 TNum<- typeofMonad g e';
+typeof g (Between s m e) = do{TNum<- typeof g s;
+                                 TNum<- typeof g m;
+                                 TNum<- typeof g e;
                                  return TBool}
 
-typeofMonad g (Fix x) = 
+typeof g (Fix x) = do {(d :->: r) <- typeof g x;
+                        return r}
 
-typeofMonad g (Bind i v b) = do {tv <- typeofMonad g v;
-                                 typeofMonad (i,tv):g b}
+typeof g (Bind i v b) = do {tv <- typeof g v;
+                            typeof (i,tv):g b}
 
-typeofMonad _ = Nothing
+typeof _ _ = Nothing
 
 
 -- Part 1: Scoping
@@ -214,7 +213,7 @@ eval e (If0 c t f) = do {
 
 eval e (Id id) = lookup id e
 
-eval e (Lambda i b) = return (ClosureV i b e)
+eval e (Lambda i d b) = return (ClosureV i b e)
 
 eval e (App f a) = do {
     (ClosureV i b j) <- eval e f;
@@ -267,70 +266,10 @@ eval e (Fix f) = do {
   eval j (subst i (Fix (Lambda i b)) b)}
 
 --Type Inference
--- typeInfer :: TypeEnv -> KULang -> Maybe KUTypeLangVal
-
-
---Reader Monad
-evalReader :: KULang -> Reader EnvVal KULangVal
-evalReader (Num x) = if x<0 then error "fail" else return (NumV x)
-
-evalReader (Plus l r) = do {
-  NumV x <- evalReader l;
-  NumV y <- evalReader r;
-  return (NumV (x+y))
-}
-
-evalReader (Minus l r) = do {
-  NumV x <- evalReader l;
-  NumV y <- evalReader r;
-  if (x-y)<0 then error "fail" else return (NumV (x-y))
-}
-
-evalReader (Mult l r) = do {
-  NumV x <- evalReader l;
-  NumV y <- evalReader r;
-  return (NumV (x*y))
-}
-
-evalReader (Div l r) = do {
-  NumV x <- evalReader l;
-  NumV y <- evalReader r;
-  if y == 0 then error "fail" else return (NumV (x `div` y)) 
-}
-
-evalReader (Exp l r) = do {
-  NumV x <- evalReader l;
-  NumV y <- evalReader r;
-  return (NumV (x^y))
-}
-
-evalReader (If0 c t f) = do {
-    c' <- evalReader c;
-    if c' == NumV 0 then evalReader t else evalReader f;
-} 
-
-evalReader (Id id) = do {
-  env <- ask;
-    case lookup id env of 
-      Nothing -> error "fail"
-      Just x -> return x
-}
-
-evalReader (Lambda i b) = do {
-  env <- ask;
-  return (ClosureV i b env)
-}
-
-evalReader (App f a) = do {
-  (ClosureV i b e) <- evalReader f;
-  v <- evalReader a;
-  local (useClosure i v e) (evalReader b)
-}
+typeInfer :: Gamma -> KULang -> Maybe KUTypeLang
+typeInfer expr = typeof [] expr
 
 --Interpretation 
 interpret :: KULang -> (Maybe KULangVal)
--- interpret expr = if typeofMonad [] expr == Nothing then Nothing else evalDeferred [] expr
+-- interpret expr = if typeof [] expr == Nothing then Nothing else evalDeferred [] expr
 interpret str = eval [] str
-
--- interpReader :: KULang -> KULangVal
--- interpReader x = runR (evalReader x) []
